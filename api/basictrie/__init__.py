5895f6a18084e0445fb17a7e1ea7abf0e8e9c71a
Tyler Boright <tyler.boright@gmail.com>
2020-08-04 19:06:31 +0800

    adds basic class with only two methods because packages I searched for did not have simple insert / get longest prefix methods that didn't involve direct assignation of strings; adds unit tests to ensure the package will meet our needs

---
+++ api/basictrie/__init__.py
@@ -0,0 +1,54 @@
+class TrieNode(object):
+    """
+    Basic Trie node implementation. Focused around inserting and checking prefixes only.
+    """
+    def __init__(self, char: str):
+        self.char= char
+        self.children = []
+
+        self.word_finished = False
+
+        self.counter = 1
+
+def add(root, word: str):
+    """
+    Add a word to the trie structure
+    """
+    node = root
+    for char in word:
+        found_in_child = False
+        for child in node.children:
+            if child.char == char: 
+                node = child
+                found_in_child = True
+                break
+
+        if not found_in_child: 
+            new_node = TrieNode(char)        
+            node.children.append(new_node)
+
+            # point to new node to continue nesting structure
+            node = new_node
+
+    node.word_finished = True
+
+def longest_prefix(root, word: str) -> str:
+    prefix = ''
+    node = root
+
+    # if the trie is empty, return the entire word as longest prefix
+    if not node.children:
+        return word
+
+    for char in word:
+        child_count = len(node.children)
+        # ignore the first character of a trie
+        if child_count > 1 and node.char != '*':
+            return prefix
+        
+        for child in node.children:
+            if child.char == char:
+                node = child
+                prefix += char
+                break
+    return prefix
--- api/test.py
+++ api/test.py
@@ -2,6 +2,7 @@ import json
 import unittest
 
 from parsenames import toJson
+import basictrie as trie
 
 class TestParseNames(unittest.TestCase):
     def test_toJson(self):
@@ -11,6 +12,20 @@ class TestParseNames(unittest.TestCase):
         strList = ['cool_apple', 'thatApple', 'cool_orange', 'thatOrange', 'cool_banana', 'cool_kiwi', 'thatKiwi', 'alaska']
         expected = json.dumps({'cool_': ['cool_apple', 'cool_orange', 'cool_banana', 'cool_kiwi'], 'that': ['thatApple', 'thatOrange', 'thatKiwi'], 'alaska': ['alaska']})
         self.assertEqual(toJson(strList), expected)
+
+class TestBasicTrie(unittest.TestCase):
+    def test_longest_prefix(self):
+        root = trie.TrieNode('*')
+        trie.add(root, 'cool_runnings')
+        trie.add(root, 'cool__runnings')
+        trie.add(root, 'cool_')
+        trie.add(root, 'fantabulous')
+        self.assertEqual(trie.longest_prefix(root, 'cool_runnings'), 'cool_')
+        self.assertEqual(trie.longest_prefix(root, 'cool__runnings'), 'cool_')
+        self.assertEqual(trie.longest_prefix(root, 'cool_'), 'cool_')
+        self.assertEqual(trie.longest_prefix(root, 'coolRunnings'), 'cool')
+        self.assertEqual(trie.longest_prefix(root, 'fantabulous'), 'fantabulous')
+
 
 if __name__ == "__main__":
     unittest.main()
